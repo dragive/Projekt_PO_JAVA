ce29f28909d00904f5face297bd764848f9e5f8b
BuildTools <maciejf@int.pl>
2020-05-04 23:29:33 +0200

    Dodanie pierwszych podstawowych plików projektu na gałęzi do testowania zgłaszania commitów i pobrania projektu

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.io.Console;
+//java jest lekko mowiac slaba ~maciek
+public class Main {
+
+    public static void main(String[] args) {
+        test x= new test();
+    }
+}
+/*
+private : prywatne
+pbulic -*-
+protected: ??????????????????
+    public w obrebie apkietu
+         i w kazdej kklasie dizedziczacej
+pakietowy czyli nic rozszerza protected a protected wierze i pozwala tylko w "tym samym folderze" i chuj
+ */
